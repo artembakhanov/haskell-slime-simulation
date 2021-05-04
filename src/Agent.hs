f1329ac420bf6c08cab4c1ae750e990f4f600fe2
Alfiya <alphiya01@mail.ru>
2021-05-04 20:08:23 +0300

    Add agent list to matrix function

--- src/Agent.hs
+++ src/Agent.hs
@@ -1,39 +1,27 @@
-module Agent
-    (Agent) where
+module Agent where
 
 import Data.Array.Accelerate                              as A
 import qualified Prelude                                  as P
 
-data Agent = Agent (Vector (Int)) Float
-type AgentElt = (Int, Int, Float)
+type Agent = ((Int, Int), Float)
 
+fromAgentToShape :: Exp Agent -> Exp ((:.) ((:.) Z Int) Int)
+fromAgentToShape agent = I2 (fst (fst agent)) (snd (fst agent))
 
-fromAgentsToMatrix :: (width, height) -> Acc (Vector AgentElt) -> Acc (Matrix Float)
-fromAgentsToMatrix (width, height) Z = fromList (Z:.width:.height) (repeat 0)
-fromAgentsToMatrix (width, height) agents = _
-  where
-    matrix = fold (op) 
-    matrix[] = 
+fromAgentsToMatrix :: (Int, Int) -> Acc (Array DIM1 Agent) -> Acc (Matrix Float)
+fromAgentsToMatrix (width, height) agents =
+    let
+        zeros = fill (constant (Z:.width:.height)) 0
+        ones  = fill (I1 (size agents))            1          -- which shape is it?
+    in
+        permute (+) zeros (\ix -> Just_ (fromAgentToShape (agents!ix))) ones
 
-initAgentsVector n (width, height) = 
-  fromList (Z:.n) (initAgentsElt n (width, height))
-
-
-initAgentsElt :: Int -> (Int, Int) -> [AgentElt]
-initAgentsElt 0 (width, height) = []
-initAgentsElt n (width, height) = 
-  (x_, y_, 0) : initAgentsElt (n - 1) (width, height)
-  	where
-      a  = P.fromIntegral (n `P.mod` 100)
-      x_ = P.floor (a P./ 100 P.* P.fromIntegral (width))
-      y_ = P.floor (a P./ 100 P.* P.fromIntegral (height))
+initAgentsElt n (width, height) = fromList (Z:.n) (initAgents n (width, height))
 
 initAgents :: Int -> (Int, Int) -> [Agent]
-
 initAgents 0 (width, height) = []
-initAgents n (width, height) = 
-  (Agent (fromList (Z:.2) [x_, y_]) 0) : initAgents (n - 1) (width, height)
+initAgents n (width, height) = ((x_, y_), 0) : initAgents (n - 1) (width, height)
     where
-      a  = P.fromIntegral (n `P.mod` 100)
-      x_ = P.floor (a P./ 100 P.* P.fromIntegral (width))
-      y_ = P.floor (a P./ 100 P.* P.fromIntegral (height))
+        a  = P.fromIntegral (n `P.mod` 100)
+        x_ = P.floor (a P./ 100 P.* P.fromIntegral (width))
+        y_ = P.floor (a P./ 100 P.* P.fromIntegral (height))
