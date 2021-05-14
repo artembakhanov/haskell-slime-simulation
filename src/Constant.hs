52d76e2c7774e21a0cc3d9e4a57fca53f442993f
artembakhanov <artembakhanov@gmail.com>
2021-05-15 01:20:34 +0300

    Update Constant.hs

--- src/Constant.hs
+++ src/Constant.hs
@@ -5,14 +5,17 @@ import qualified Prelude                                  as P
 import Data.Array.Accelerate.System.Random.MWC
 
 -- gaussian = [constant (0.06136),constant 0.24477, constant 0.38774, constant 0.24477, constant 0.06136]
+-- gaussian :: [Exp Float]
+-- gaussian = [constant 0.7, constant 0.8, constant 0.9, constant 0.8, constant 0.7]
+
 gaussian :: [Exp Float]
-gaussian = [constant 0.7, constant 0.8, constant 0.9, constant 0.8, constant 0.7]
+gaussian = [constant 0.2, constant 0.2, constant 0.2, constant 0.2, constant 0.2]
 
 -- trail coefficient
 trailWeight, decayRate, dist, rotation, speed, diffuseRate :: Exp Float
 trailWeight = 5
 decayRate = 0.2
-dist = 4.0
+dist = 15
 rotation = pi / 6
 speed = 10
 diffuseRate = 3.0
